20c39cf533343bf9873cf752fe5fd4f102a3f934
mnetna <sssolee90@gmail.com>
2020-04-22 01:11:13 +0900

    add 기능 추가

--- src/App.js
+++ src/App.js
@@ -1,13 +1,60 @@
-import React from 'react';
+import React, { useState } from 'react';
 import SalesTable from './SalesTable';
+import { MdKeyboardArrowLeft, MdKeyboardArrowRight} from 'react-icons/md';
 
 function App() {
+  const [date, setDate] = useState(new Date());
+
+  const prevDate = () => {
+    let year = date.getFullYear();
+    let month = (date.getMonth()+1)-1;
+    
+    if ( month === 0 ) {
+      year = year-1;
+      month = 12;
+    }
+
+    setDate(new Date(year, month-1));
+  }
+
+  const nextDate = () => {
+    let year = date.getFullYear();
+    let month = (date.getMonth()+1)+1;
+    
+    if ( month === 13 ) {
+      year = year+1;
+      month = 1;
+    }
+
+    setDate(new Date(year, month-1));
+  }
+
   return (
-    <>
-    <h3>매출을 관리해보즈아~</h3>
-    <SalesTable></SalesTable>
-    </>
+    <div>
+      <h3>매출을 관리해보즈아~</h3>
+      <div>
+        <MdKeyboardArrowLeft size={30} onClick={prevDate}/> {date.getMonth()+1} <MdKeyboardArrowRight size={30} onClick={nextDate}/>
+      </div>
+      <div>
+        <SalesTable year={date.getFullYear()} month={date.getMonth()+1}> </SalesTable>
+      </div>
+    </div>
   );
 }
 
-export default App;
+export default React.memo(App);
+
+
+// TODO 왜 안되는지 찾기!
+// let product = products.slice(rowIndex, rowIndex+1)[0];
+// product.selected = true;
+
+// setProducts(products => ({
+//   ...products,
+//   products: [
+//     ...products.slice(0, rowIndex),
+//     product
+//     //...products.slice(rowIndex+1)
+//   ]
+//  })
+// )
--- src/SalesTable.js
+++ src/SalesTable.js
@@ -1,166 +1,316 @@
 import React, { useState, useEffect} from 'react';
+import axios from 'axios';
 import BootstrapTable from 'react-bootstrap-table-next';
 import cellEditFactory from 'react-bootstrap-table2-editor';
-import { MdControlPoint, MdDeleteForever } from 'react-icons/md';
+import { MdAddCircleOutline, MdRemoveCircleOutline } from 'react-icons/md';
 import 'react-bootstrap-table-next/dist/react-bootstrap-table2.min.css';
 
-function SalesTable() {
-  const columns = [
-    {
-      dataField: 'date',
-      text: '날짜',
-      events: {
-        onMouseOver: (e, column, columnIndex, row, rowIndex) => {
-          console.log("Mouse Over!!")
-          console.log(column);
-          console.log(columnIndex);
-        }
-      },
-      editable: false
-    }, {
-      dataField: 'tableNum',
-      text: '테이블수',
-      editable: true
-    }, {
-      dataField: 'cardSales',
-      text: '카드 매출',
-      editable: true
-    }, {
-      dataField: 'cashSales',
-      text: '현금 매출',
-      editable: true,
-    }, {
-      dataField: 'serviceSales',
-      text: '서비스 매출',
-      editable: true
-    }, {
-      dataField: 'totalSales',
-      text: '총 매출',
-      editable: false
-    }, {
-      dataField: 'unitPrice',
-      text: '객단가',
-      editable: false
-    },{
-      dataField: 'action',
-      //isDummyField: true,
-      text: '-',
-      formatter: (cellContent, row) => {
-       return (
-         <MdDeleteForever onClick={() => onRemove(row.date)}/>
-       )
-      },
-      editable: false
-    },
-  ];
+const columns = [
+  {
+    dataField: 'saleDate',
+    text: '날짜',
+    editable: false,
+    headerAlign: 'center'
+  }, {
+    dataField: 'tableCount',
+    text: '테이블 수',
+    editable: true,
+    headerAlign: 'center'
+  }, {
+    dataField: 'cardSales',
+    text: '카드 매출',
+    editable: true,
+    headerAlign: 'center',
+    formatter: priceFormatter
+  }, {
+    dataField: 'moneySales',
+    text: '현금 매출',
+    editable: true,
+    headerAlign: 'center',
+    formatter: priceFormatter
+  }, {
+    dataField: 'serviceSales',
+    text: '서비스 매출',
+    editable: true,
+    headerAlign: 'center',
+    formatter: priceFormatter
+  }, {
+    dataField: 'totalSales',
+    text: '총 매출',
+    editable: false,
+    headerAlign: 'center',
+    formatter: priceFormatter
+  }, {
+    dataField: 'unitPrice',
+    text: '객단가',
+    editable: false,
+    headerAlign: 'center',
+    formatter: priceFormatter
+  }, {
+    dataField: 'modified',
+    text: '수정여부',
+    editable: false
+  }, {
+    dataField: 'selected',
+    text: '선택여부',
+    editable: false
+  }
+];
 
-  const [products, setProducts] = useState ([
-    {
-      date: '2020-01-01',
-      tableNum: '5',
-      cardSales: '500000',
-      cashSales: '300000',
-      serviceSales: '10000',
-      totalSales: 0,
-      unitPrice: 0,
-      active: false
-    },{
-      date: "2020-01-02",
-      tableNum: '20',
-      cardSales: "1000000",
-      cashSales: "100000",
-      serviceSales: "50000",
-      totalSales: 0,
-      unitPrice: 0,
-      active: false
+function priceFormatter(cell, row) {
+  const regexp = /\B(?=(\d{3})+(?!\d))/g;
+  return <span> {cell > 0 ? Math.ceil(Number(cell)).toString().replace(regexp, ',') : cell} </span>;
+}
+
+function SalesTable({year, month}) {
+
+  const [sales, setSales] = useState(null);
+  const [init, setInit] = useState(false);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
+  const [average, setAverage] = useState ({
+    averageTableCount: '',
+    averageCardSales: '',
+    averageMoneySales: '',
+    averageServiceSales: '',
+    averageUnitPrice: '',
+    montlyTotalSales: ''
+  });
+
+  const fetchSales = async () => {
+    try {
+      setError(null);
+      setSales(null);
+      setLoading(true);
+      const response = await axios.get(
+        'http://ctk0327.iptime.org:8080/sales', {
+          params: {
+            saleYear: year,
+            saleMonth: month
+          }
+        });
+      setSales(response.data);
+    } catch (e) {
+      setError(e);
     }
-  ]);
+    setLoading(false);
+    setInit(true);
+  };
 
-  useEffect(() => {
-    console.log("컴포넌트가 화면에 나타남");
-    console.log(products);
-     // active가 true인 것만 행 색 변하기
-    return () => {
-      console.log("컴퓨넌트가 화면에서 사라짐");
-      console.log(products);
-    };
-  }, [products]);
+  const saveSales = async () => {
+    const saveSalesList = sales.filter(sale => sale.modified === true);
+    console.log(JSON.stringify(saveSalesList));
 
-  //const CaptionElement = () => <h3 style={{ borderRadius: "0.25em", textAlign: "center", color: "purple", border: "1px solid purple", padding: "0.5em" }}>매출 관리</h3>;
+    try {
+      const response = await axios.post(
+        'http://ctk0327.iptime.org:8080/sales', {body: JSON.stringify(saveSalesList)}
+        );
+    } catch (e) {
+      console.log(e.response.data.message);
+    }
+  };
 
-  const updateCellValue = (key, value, dataField) => {
-    setProducts(
-      products.map(product => 
-        product.date === key ? {
-          ...product, 
-          [dataField]: value, 
-          totalSales: Number(product.cardSales)+Number(product.cashSales)+Number(product.serviceSales),
-          unitPrice: Number(product.totalSales)/Number(product.tableNum),
-          active: true
-        } : product,
+  const calculate = () => {
+    const calculateTotalSales = (cardSales, moneySales, serviceSales) => {
+      return Number(cardSales)+Number(moneySales)+Number(serviceSales);
+    }
+  
+    const calculateUnitPrice = (totalSales, tableCount) => {
+      return totalSales > 0 ? Number(totalSales)/Number(tableCount) : 0;
+    }
+
+    setSales(
+      sales.map(sale =>
+        true ?
+        { ...sale,
+          totalSales : calculateTotalSales(sale.cardSales, sale.moneySales, sale.serviceSales),
+          unitPrice : calculateUnitPrice(calculateTotalSales(sale.cardSales, sale.moneySales, sale.serviceSales), sale.tableCount)
+        } : sale,
       )
     )
   }
 
-  const onCreate = () => {
-    console.log("배열 추가");
+  const calculateAllAvrage = (arrSize) => {
+    const calculateSum = (arrName) => {
+      return sales.reduce((acc, currValue) => {
+        if (arrName === 'tableCount')
+          return Number(acc)+Number(currValue.tableCount);
+        else if (arrName === 'cardSales')
+          return Number(acc)+Number(currValue.cardSales);
+        else if (arrName === 'moneySales')
+          return Number(acc)+Number(currValue.moneySales);
+        else if (arrName === 'serviceSale')
+          return Number(acc)+Number(currValue.serviceSale);
+        else if (arrName === 'unitPrice')
+          return Number(acc)+Number(currValue.unitPrice);
+        else if (arrName === 'totalSales')
+          return Number(acc)+Number(currValue.totalSales);
+      }, 0)
+    }
 
-    const product = {
-      date: "2020-01-03",
-      tableNum: "0",
+    const calculateAverage = (sum, arrSize) => {
+      return sum > 0 ? Number(sum)/Number(arrSize) : 0;
+    }
+
+    setAverage({
+      ...average,
+      averageTableCount: calculateAverage(calculateSum('tableCount'), arrSize),
+      averageCardSales: calculateAverage(calculateSum('cardSales'), arrSize),
+      averageMoneySales: calculateAverage(calculateSum('moneySales'), arrSize),
+      averageServiceSales: calculateAverage(calculateSum('serviceSale'), arrSize),
+      averageUnitPrice: calculateAverage(calculateSum('unitPrice'), arrSize),
+      montlyTotalSales: calculateSum('totalSales'),
+    })
+  }
+
+  useEffect(() => { 
+    fetchSales();
+  }, [month]);
+
+  useEffect(() => {
+    if(sales != null && init)
+      calculate(); setInit(false);
+  }, [init]);
+
+  useEffect(() => {
+    if(sales != null) 
+      calculateAllAvrage(sales.length);
+  }, [sales]);
+
+  if (loading) return <div>로딩중..</div>;
+  if (error) return <div>에러가 발생했습니다</div>;
+  if (!sales) return null;
+
+  /* sales 배열 추가 */
+  const onCreate = () => {
+    let lastDate = new Date(sales[sales.length-1].saleDate);
+    let today = new Date();
+
+    if(lastDate.getDate() === today.getDate()) {
+      alert("오늘 날짜까지만 추가할 수 있습니다.");
+      return;
+    } 
+
+    lastDate.setDate(lastDate.getDate()+1);
+
+    if(lastDate.getDate() === 1) {
+      alert("이 달의 마지막 날입니다.");
+      return;
+    } 
+
+    const sale = {
+      saleDate: lastDate.getFullYear() + "-" + ("0"+(lastDate.getMonth()+1)).slice(-2) + "-" + ("0"+lastDate.getDate()).slice(-2),
+      tableCount: "0",
       cardSales: "0",
-      cashSales: "0",
+      moneySales: "0",
       serviceSales: "0",
       totalSales: 0,
       unitPrice: 0,
-      active: true
+      modified: true,
     }
 
-    setProducts(products.concat(product));
+    setSales(sales.concat(sale));
   }
 
-  const onRemove = (date) => {
-    console.log("배열 삭제");
-    //console.log(products);
-    setProducts(
-      products.map(product => 
-        product.date === date ? {
-          ...product, 
-          tableNum: 0,
+  /* 휴지통 아이콘을 클릭 시 선택한 row를 0으로 초기화 */
+  const onRemove = (sales) => {
+    setSales(
+      sales.map(sale => 
+        sale.selected ? {
+          ...sale, 
+          tableCount: 0,
           cardSales: 0,
-          cashSales: 0,
+          moneySales: 0,
           serviceSales: 0,
           totalSales: 0,
           unitPrice: 0,
-          active: true
-        } : product,
+          modified: true
+        } : sale,
       )
     )
-    //console.log(products);
   }
 
+  /* Cell Update시 이벤트 설정 */
+  const updateCellValue = (key, dataField, value) => {
+    setSales(
+      sales.map(sale => 
+        sale.saleId === key ? {
+          ...sale, 
+          [dataField]: value, 
+          modified: true
+        } : sale,
+      )
+    )
+  }
+
+  /* SelectBox 선택시 sales 배열의 특정 원소의 selected 값을 true로 변경 */
+  const selectRow = {
+    mode: 'checkbox',
+    clickToSelect: false,
+    clickToEdit: true,
+
+    onSelect: (row, isSelect, rowIndex, e) => {
+      if(isSelect) {
+        setSales(
+          sales.map(sale => 
+            sale.saleId === row.saleId ? {
+              ...sale, 
+              selected: true
+            } : sale
+          )
+        )
+      } else {
+        setSales(
+          sales.map(sale => 
+            sale.saleId === row.saleId ? {
+              ...sale, 
+              selected: false
+            } : sale
+          )
+        )
+      }
+    }
+  };
+
+  /* sales 배열의 원소가 수정된 경우 특정 색으로 변경 */
+  const rowStyle = (row, rowIndex) => {
+    const style = {};
+    if (row.modified) {
+      style.backgroundColor = '#c8e6c9';
+    } else {
+      style.backgroundColor = '#FFFFFF';
+    }
+
+    return style;
+  }
+  
   return (
-    <>
-      <BootstrapTable 
-        keyField="date" 
-        data={ products } 
-        hover
-        columns={ columns } 
-        //caption={<CaptionElement />} 
-        cellEdit={ cellEditFactory({ 
-          mode: "click",
-          afterSaveCell: (oldValue, newValue, row, column) => { 
-            updateCellValue(row.date, newValue, column.dataField);
-          }
-        }) }
-      />
-      <MdControlPoint onClick={onCreate}/>
-    </>
+    <div>
+      <div>
+        평균 테이블 수: {priceFormatter(average.averageTableCount)} 평균 카드 매출 : {priceFormatter(average.averageCardSales)} 평균 현금 매출 : {priceFormatter(average.averageMoneySales)} 평균 서비스 매출 : {priceFormatter(average.averageServiceSales)} 평균 객 단가 : {priceFormatter(average.averageUnitPrice)} 총 매출액: {priceFormatter(average.montlyTotalSales)} 
+      </div>
+      <div>
+        <BootstrapTable 
+          keyField='saleId'
+          data={ sales } 
+          columns={ columns }  
+          selectRow={ selectRow }
+          rowStyle={ rowStyle }
+          cellEdit={ cellEditFactory({ 
+            mode: 'click',
+            afterSaveCell: (oldValue, newValue, row, column) => { 
+              updateCellValue(row.saleId, column.dataField, newValue, oldValue);
+            }
+          }) }
+        />
+      </div>
+      <div>
+        <MdAddCircleOutline size={30} onClick={onCreate}/>
+        <MdRemoveCircleOutline size={30} onClick={() => onRemove(sales)}/>
+        <button onClick={saveSales}>저장</button>
+      </div>
+    </div>
   );
 }
 
 export default SalesTable;
-
-// TODO Mock 데이터 연동 - 조회 및 저장
-// TODO 변경된 값은 색 바꾸기
-// TODO 삭제 아이콘 클릭시 배열 초기화
